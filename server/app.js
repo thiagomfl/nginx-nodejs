66491b682d131900c979367084635a2b285fdc71
Thiago <thmoura14@hotmail.com>
2021-09-27 09:16:13 -0300

    updated to fix, reviewer comments

--- server/app.js
+++ server/app.js
@@ -13,6 +13,15 @@ const config = {
 
 const conn = mysql.createConnection(config);
 
+const table =
+  "CREATE TABLE people(id int not null auto_increment, name varchar(255) not null, primary key(id))";
+
+conn.query(table)
+
+const insert = `INSERT INTO people (name) values ('Thiago')`
+
+conn.query(insert)
+
 const sql = `SELECT * FROM people`;
 
 let result;
